df3f3fac8a82c70b064bc0e9263b3d3dbb960dca
Ian JCV <ausp.jcv@gmail.com>
2025-08-17 10:56:56 +0200

    move -O3 to noconsole build

--- build.bat
+++ build.bat
@@ -3,6 +3,6 @@
 set winlibs=-lwinmm -lgdi32 -luser32 -lkernel32 -lshell32
 set rtlibs=-lmsvcrt -lucrt -lmsvcprt -lvcruntime -llibcmt -Xlinker /NODEFAULTLIB
 
-if [%1]==[noconsole] set rtlibs=%rtlibs% -Xlinker /SUBSYSTEM:WINDOWS -Xlinker /ENTRY:mainCRTStartup
+if [%1]==[noconsole] set rtlibs=%rtlibs% -Xlinker /SUBSYSTEM:WINDOWS -Xlinker /ENTRY:mainCRTStartup -DNDEBUG -O3
 
-clang -o todo.exe -O3 -std=c23 main.c -Llib -lraylib %winlibs% %rtlibs% -D_CRT_SECURE_NO_WARNINGS
+clang -o todo.exe -std=c23 main.c -Llib -lraylib %winlibs% %rtlibs% -D_CRT_SECURE_NO_WARNINGS
